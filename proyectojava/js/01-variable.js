32c619fa23017fb720225c32d2687b3811edc007
fullstack <daniroher97@gmail.com>
2023-10-09 14:26:10 +0200

    portfolio

---
+++ proyectojava/js/01-variable.js
@@ -0,0 +1,25 @@
+
+
+// alert("Hola mundo");
+
+let producto = "Nombre del producto";
+let disponible;
+
+disponible = true;
+
+disponible = "No hay producto";
+
+let camiseta = "red",
+pantalon = "yellow",
+zapatos;
+
+zapatos="Negros"
+
+console.log("comentario CAMISETA:",camiseta);
+console.log("comentario pantalon:",pantalon);
+console.log("comentario zapatos:",zapatos);
+
+
+const constante = "Valor Constante";
+console.log("comentario constante:", constante)
+
---
+++ proyectojava/js/02-tipos-de-datos.js
@@ -0,0 +1,36 @@
+const producto1= "Juego de mesa EL PUEBLO DUERME";
+const producto2= String("DOOBLE");
+const producto3 = new String("UNO"); 
+
+console.log ("producto1", producto1);
+console.log("producto2", producto2);
+console.log("producto3", producto3);
+
+console.log ("producto1", typeof producto1);
+console.log("producto2", typeof producto2);
+console.log("producto3", typeof producto3);
+
+let numero100 = false,
+numero200 = "200";
+
+
+console.log("numero100", typeof numero100);
+console.log("numero200", typeof numero200);
+
+
+console.log("metodo lenght", producto1.length);
+
+console.log("metodo indexof", producto1.indexOf("PUEBLO"));
+
+console.log("metodo includes", producto1.includes("PUEBLO"));
+
+
+let ejstring1= "ejercicio primero string",
+ejstring2 ="ejemplo de ejercicio",
+ejstring3= "ejercicio string"
+
+console.log("metodo lenght", ejstring1.length);
+
+console.log("metodo indexof", ejstring2.indexOf("ejercicio"));
+
+console.log("metodo includes", ejstring3.includes("ejercicio"));
